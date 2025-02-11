390bbe9e8b92274e2d8c0d85be94c3d3a2479959
MoHusaynn <mohussainn.work@gmail.com>
2025-02-11 13:07:39 +0300

    Update supabase.js
    Removed Supabase KEY & URL

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://klzifxxrckljxgoojoow.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsemlmeHhyY2tsanhnb29qb293Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5MzY0NzcsImV4cCI6MjA1MjUxMjQ3N30.A6y0aIf3jOn-l4EwfauYX4HW659r91e0cxuCLmG7bfQ'
+const supabaseUrl = ''
+const supabaseKey = ''
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
